eaa4281de978106e2b497e2a07f20c77b892c9db
Samir Simon Abdallah Abdul Nour <samir.simon.a.a.nour@gmail.com>
2026-02-19 08:45:45 -0300

    Add files via upload

---
+++ aula2/exemplo02.php
@@ -0,0 +1,11 @@
+<?php
+    //Verificar conteudo e tipos de variavel
+    $x = 10.5;
+    echo "O valor de x é: $x <br>";
+    echo "O valor de x é: ". $x; //CONTENAÇÃO
+    echo "<hr>";
+
+     // DEPURAÇÃO de código
+    var_dump($x);
+    print_r($x);
+?>
---
+++ aula2/exemplo05.php
@@ -0,0 +1,15 @@
+<?php
+    $dia = "segunda";
+    switch ($dia){
+        case "segunda":
+            echo "Inicio da semana!";
+            break;
+        case "sexta":
+            echo "Fim de semana chegando";
+            break;
+        default:
+            echo "Dia normal";
+        
+        }
+
+?>
