e302b94ac14b815e88d6fe718f44be19bf4faf94
Samir Simon Abdallah Abdul Nour <samir.simon.a.a.nour@gmail.com>
2026-04-09 11:33:52 -0300

    Add files via upload

---
+++ aula06/erro.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <section>
+        <h1 class="meuh1" >:(</h1>
+        <h2>O PC encontrou um problema e precisa de ser reiniciado.</h2>
+        <h2> Estamos apenas a recolher algumas informações de erro e, em seguida, iremos reiniciar</h2>
+        <h3>you</h3>
+        <h2>20% automaticamente. concluído</h2>
+    </section>
+    <form action="forms.php" method="POST">
+        <input class="butao" type="submit" value="VOLTAR" class="btn btn-primary">
+    </form>
+</body>
+</html>
