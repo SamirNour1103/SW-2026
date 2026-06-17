6e04bf6cbbb44dc94900df3a94a3969279e5f6a0
Samir Simon Abdallah Abdul Nour <samir.simon.a.a.nour@gmail.com>
2026-06-17 16:04:39 -0300

    Add files via upload

---
+++ aula09_funcao/exercicio04.php
@@ -0,0 +1,13 @@
+<?php
+    function verificarAcesso($senhaDigitada){
+        switch ($senhaDigitada) {
+            case '123456':
+                echo "Acesso Liberado";
+                break;
+            default:
+                echo "Acesso Negado";
+                break;
+        }
+    }
+    verificarAcesso("123456");
+?>
